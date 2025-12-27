d920c0c56ba9deeec078437c2b127a2d211bd475
ahmetgultekin <251647744+ahmet-gultekin@users.noreply.github.com>
2025-12-27 17:03:57 +0300

    Update controller.js

--- src/modules/users/controller.js
+++ src/modules/users/controller.js
@@ -1,1 +1,15 @@
+exports.createUser = (req, res) => {
+  const { name, email } = req.body;
+
+  if (!name || !email) {
+    return res.status(400).json({
+      message: "Name and email are required",
+    });
+  }
+
+  return res.status(201).json({
+    message: "User created",
+    user: { name, email },
+  });
+};
 
